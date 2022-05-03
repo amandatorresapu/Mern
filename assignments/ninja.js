cfcaf41270c8899d3087665d86c49bbdfba7f343
Amanda Torres <amandatorres-brooks@Amandas-MacBook-Pro.local>
2022-05-02 22:32:21 -0700

    update

--- assignments/ninja.js
+++ assignments/ninja.js
@@ -24,9 +24,28 @@ class Ninja {
         this.health += 10;
         return this;
     }
-    
-}
 
+//     parentFunction(){
+//         return "This is coming from the parent!";
+//     }
+// }
+// // child M5 class
+// class sensi extends Ninja {
+//     constructor(color) {
+//         super("BMW", "M5", color);
+//     }
+//     // simple function in the child class
+//     childFunction() {
+//         // by using super, we can call the parent method
+//         const message = super.parentFunction();
+//         console.log(message);
+//     }
+// }
+// const m5 = new M5("Blue");
+// m5.childFunction();
+    
+// }
+}
     const ninja1 = new Ninja("meow meow");
     ninja1.sayName().showStats().drinkSake().showStats();
     console.log(ninja1)
