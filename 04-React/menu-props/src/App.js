b94e87c67c528d9694fe8743cd25e74b6720d8ec
Amanda Torres <amandatorres-brooks@Amandas-MacBook-Pro.local>
2022-05-04 15:07:41 -0700

    new react menu project

--- 04-React/menu-props/src/App.js
+++ 04-React/menu-props/src/App.js
@@ -7,7 +7,14 @@ function App() {
   return (
     <div className="App">
       <h1>hello welcome to the dojo diner</h1>
-      <Menu/>
+      
+      <Menu name={"mac and cheese"} price={5} cals={"300"}><p><em>*its friedn squid with basil</em></p></Menu>
+      <Menu name={"calimari"} price={2.99} cals={"350"}><p>fish are friends not food</p></Menu>
+      <Menu name={"cheese burger"}price={3.99} cals={"100"}><p>nector of the gods</p></Menu>
+      <Menu name={"sushi"}price={7.99} cals={"400"}>
+      <p>its friedn squid with basil</p></Menu>
+      
+      
     </div>
   );
 }
--- 04-React/menu-props/src/components/Menu.jsx
+++ 04-React/menu-props/src/components/Menu.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
 class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            price: this.props.price
+        };
+    }
+
+    
+
+
     render (){
-        return <div>
-            <h4>This is the menu components</h4>
+        //put the let with all the props, so you dont have to put it with the name as this.props.name, can just be nameß
+        let {name, price,cals, children} = this.props;
+
+      const increasePrice=()=>{
+            this.state.price++;
+            this.setState({ price: this.state.price })
+        }
+        
+        return (<div>
+            <h4>Item Name: {name} </h4>
+            <p>Price: {this.state.price} </p>
+            <p>Calories:{cals} </p>
+            {children}
+            {/* <button onClick={()=> this.setState({price: this.state.price+1})}>Inflation for {name}</button> another way to put the function in the button */}
+            <button onClick={increasePrice}>Inflation for {name}</button>
         </div>
+        )
     }
 }
 
