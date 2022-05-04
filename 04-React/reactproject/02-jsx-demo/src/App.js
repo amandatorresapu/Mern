5ea39487e37eed5e97bd951562ce38419b8c13bb
Amanda Torres <amandatorres-brooks@Amandas-MacBook-Pro.local>
2022-05-04 09:32:03 -0700

    new react

---
+++ 04-React/reactproject/02-jsx-demo/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> To do list</h1>
+      
+        <li>Finish core assignements</li>
+        <li>Drink water</li>
+        <li>Go to the gym</li>
+        <li>Practice algos</li>
+        <li>Be a boss bitchhhhh </li>
+    
+    </div>
+  );
+}
+
+export default App;
