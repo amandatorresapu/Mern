77a3f0e49b28dfaed34f4c2ced6b2f56837bf898
Amanda Torres <amandatorres-brooks@Amandas-MacBook-Pro.local>
2022-05-04 13:29:35 -0700

    new react

---
+++ 04-React/menu-props/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Menu from './components/Menu';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello welcome to the dojo diner</h1>
+      <Menu/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 04-React/menu-props/src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+    render (){
+        return <div>
+            <h4>This is the menu components</h4>
+        </div>
+    }
+}
+
+export default Menu;
