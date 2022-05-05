0815a54d9de28b0076a223124422239dd4f5b278
Amanda Torres <amandatorres-brooks@Amandas-MacBook-Pro.local>
2022-05-05 15:55:12 -0700

    update

--- 04-React/props-up/src/App.js
+++ 04-React/props-up/src/App.js
@@ -7,11 +7,11 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      
-      < PersonCard firstName={"Amanda"}lastName={"Coffey"} age={22} hairColor={"brown"} > </PersonCard>
-      < PersonCard firstName={"Teola"}lastName={"Coffey"} age={19} hairColor={"brown"} > </PersonCard>
+      <h1>test</h1>
+      < PersonCard firstName={"Amanda"}lastName={"Coffey"} age={22}  hairColor={"brown"} > </PersonCard>
+       < PersonCard firstName={"Teola"}lastName={"Coffey"} age={19} hairColor={"brown"} > </PersonCard> 
       < PersonCard firstName={"Feona"}lastName={"Thomas"} age={38} hairColor={"Red"} > </PersonCard>
-      < PersonCard firstName={"AI"}lastName={"Volleyballcoach"} age={30} hairColor={"black"} > </PersonCard>
+      < PersonCard firstName={"AI"}lastName={"Volleyballcoach"} age={30} hairColor={"black"} > </PersonCard> 
       
     
     </div>
--- 04-React/props-up/src/components/PersonCard.jsx
+++ 04-React/props-up/src/components/PersonCard.jsx
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class PersonCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            age: this.props.age
-        };
-    }
+const PersonCard =(props)=> {
+    const {firstName, lastName, hairColor, children} = props;
+   //use state when you want a varabile to change in value and relfect that on page
+    const [age, setAge] = useState(props.age);
 
-    render (){
-        //put the let with all the props, so you dont have to put it with the name as this.props.name, can just be nameß
-        let {firstName, lastName,age, hairColor, children} = this.props;
 
-      const increaseBirthday=()=>{
-            this.state.age++;
-            this.setState({ Age: this.state.age })
-        }
-        
-        return (<div>
+    const increaseBirthday=()=>{
+                setAge(age+1)
+            }
+
+    return(
+    <>
+        <div>
           
             <h2>{lastName},{firstName} </h2>
-            <p>Age: {this.state.age}</p>
+            <p>Age: {age}</p>
             <p>Hair Color: {hairColor}</p>
             {/* <p>Price: {this.state.price} </p> */}
             {/* <p>Calories:{cals} </p> */}
@@ -28,8 +23,9 @@ class PersonCard extends Component {
             {/* <button onClick={()=> this.setState({price: this.state.price+1})}>Inflation for {name}</button> another way to put the function in the button */}
             <button onClick={increaseBirthday}>Happy birthday {firstName}, have a year</button>
         </div>
-        )
-    }
+
+        </>
+    )
 }
 
 export default PersonCard;
