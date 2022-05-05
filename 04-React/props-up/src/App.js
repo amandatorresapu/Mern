ad9521a0aa78c6910f0ae6b8038ae86bef596a8f
Amanda Torres <amandatorres-brooks@Amandas-MacBook-Pro.local>
2022-05-04 21:27:41 -0700

    personcard project

---
+++ 04-React/props-up/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      
+      < PersonCard firstName={"Amanda"}lastName={"Coffey"} age={22} hairColor={"brown"} > </PersonCard>
+      < PersonCard firstName={"Teola"}lastName={"Coffey"} age={19} hairColor={"brown"} > </PersonCard>
+      < PersonCard firstName={"Feona"}lastName={"Thomas"} age={38} hairColor={"Red"} > </PersonCard>
+      < PersonCard firstName={"AI"}lastName={"Volleyballcoach"} age={30} hairColor={"black"} > </PersonCard>
+      
+    
+    </div>
+  );
+}
+
+export default App;
